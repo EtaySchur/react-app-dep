9def4eb91333abc82d173c17388d0a7a4ce74551
Backline AI <backline-ai@backline-ai.com>
2025-06-26 12:15:34 +0000

    src/components/CustomFastField.tsx 	* Updated the interface to extend FieldConfig instead of the deprecated FastFieldConfig as recommended in the remediation plan. FastFieldConfig has been deprecated and FieldConfig should be used instead.

--- src/components/CustomFastField.tsx
+++ src/components/CustomFastField.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { FastField, FastFieldConfig } from 'formik';
+import { FieldConfig } from 'formik';
 
 // Using FastField with FastFieldConfig from formik
 // Extend FastFieldConfig with our additional HTML props
-interface CustomFastFieldProps extends FastFieldConfig<any> {
+interface CustomFastFieldProps extends FieldConfig {
   type?: string;
   placeholder?: string;
 }
